93c21d748c13ba968458e02bd76187faac12e88d
Trần Mạnh Trường <tmtruong2505@gmail.com>
2026-03-12 14:48:47 +0700

    Lab2

--- src/pages/Lab2.tsx
+++ src/pages/Lab2.tsx
@@ -1,14 +1,15 @@
 import { Table } from "antd";
 
 const columns = [
+    {title: 'ID', dataIndex: 'id'},
     {title: 'Name', dataIndex: 'name'},
     {title: 'Age', dataIndex: 'age'},
     {title: 'Major', dataIndex: 'major'},
 ];
 
 const data = [
-    {key: '1', name: 'Truong', age: 20, major: 'FE1'},
-    {key: '2', name: 'Hai', age: 21, major: 'FE2'},
+    {key: '1', id: 1, name: 'Truong', age: 20, major: 'FE1'},
+    {key: '2', id: 2, name: 'Hai', age: 21, major: 'FE2'},
 ];
 
 export default function Lab2() {
